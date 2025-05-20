843359dbae121d26d8028304eb4e5a69f6e78c35
kersoulos <ivereskov@yandex.ru>
2025-05-21 02:24:28 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv("7983192512:AAGcwy_lFSyAz9zqS6lnbPrNXnVHzp676k0")
-API_ID = os.getenv("23259632")
-API_HASH = os.getenv("159af8d1cae44c0f602a814dfda6ce14")
-SPREADSHEET_ID = os.getenv("1bTyNCvu2Vdf1NTDW4dDfrB0zcsiWLkNb7wF1vfVXTGc")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+API_ID = os.getenv("API_ID")
+API_HASH = os.getenv("API_HASH")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
